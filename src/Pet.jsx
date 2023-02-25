b2d66903d3fa5fc1298ddf47194ac474a7f5d1a6
abdoude12 <deabdou55@gmail.com>
2023-02-25 21:46:30 +0100

    switching from http to https

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 
 const Pet = ({ name, animal, breed, images, id, location }) => {
-  let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
+  let hero = "https://pets-images.dev-apis.com/pets/none.jpg";
   if (images.length) {
     hero = images[0];
   }
--- src/fetchPet.jsx
+++ src/fetchPet.jsx
@@ -1,6 +1,6 @@
 const fetchPet = async ({ queryKey }) => {
   const id = queryKey[1];
-  const apisRes = await fetch(`http://pets-v2.dev-apis.com/pets?id=${id}`);
+  const apisRes = await fetch(`https://pets-v2.dev-apis.com/pets?id=${id}`);
 
   if (!apisRes.ok) {
     throw new Error("the correspond id does not ok");
